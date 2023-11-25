f287a17cd00e5ed76cc39b5ba138ff1338cfc7fa
DimitriyArdent <109689205+DimitriyArdent@users.noreply.github.com>
2023-11-25 23:11:29 +0200

    rds addition

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { Client } = require('pg');
 
-const port = 3000;
+const port = 3001;
 app.use(cors());
+
+
+const client = new Client({
+  host: 'dummycloud.cpldgl5e8yx3.us-east-1.rds.amazonaws.com',
+  user: 'postgres',
+  port: 5432,
+  password: '3141531415',
+  database: 'dummyCloud',
+});
+
+client.connect();
+
 // Generate a random message
 const getRandomMessage = () => {
   const messages = [
@@ -20,6 +33,8 @@ const getRandomMessage = () => {
 app.get('/hello', (req, res) => {
   const randomMessage = getRandomMessage();
   res.send(randomMessage);
+    client.query('INSERT INTO messages (messages) VALUES ($1)  ', [randomMessage]);
+
 });
 
 // Start the server
