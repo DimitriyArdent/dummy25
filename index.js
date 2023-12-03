7b5720309fe4dd10f7b34a9894b54e1199ee58c4
DimitriyArdent <109689205+DimitriyArdent@users.noreply.github.com>
2023-12-03 15:40:55 +0200

    Revert "dire-dire2"
    This reverts commit 10e5684c89dbeb844f1eca7d7cd7690cdce4a43e.

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const getRandomMessage = () => {
     'Welcome to the React app!',
     'Random message here!',
     'Have a great day!',
-    'dire',
-    'dire2'
   ];
   const randomIndex = Math.floor(Math.random() * messages.length);
   return messages[randomIndex];
