c7bc34b51ce73f0b16db664627558bfc05bb8dab
DimitriyArdent <109689205+DimitriyArdent@users.noreply.github.com>
2023-12-03 15:07:00 +0200

    auxilary

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const getRandomMessage = () => {
     'Welcome to the React app!',
     'Random message here!',
     'Have a great day!',
+    'auxillary!'
   ];
   const randomIndex = Math.floor(Math.random() * messages.length);
   return messages[randomIndex];
